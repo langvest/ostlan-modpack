d8f43ac2abcc5e6cdc276135a73c6bd7096df6b9
LanGvest <mister.langvest@gmail.com>
2025-09-10 18:23:33 +0300

    Configure creative mode tabs custom icons

---
+++ kubejs/startup_scripts/src/tabs.js
@@ -0,0 +1,23 @@
+StartupEvents.modifyCreativeTab("alexscaves:primordial_caves", event => {
+    event.setIcon("alexscaves:amber_curiosity");
+});
+
+StartupEvents.modifyCreativeTab("alexscaves:abyssal_chasm", event => {
+    event.setIcon("alexscaves:immortal_embryo");
+});
+
+// StartupEvents.modifyCreativeTab("nethersdelight:main", event => {
+//     event.setIcon("nethersdelight:hoglin_trophy");
+// });
+
+StartupEvents.modifyCreativeTab("cavedelight:hybrid_tab", event => {
+    event.setIcon("alexscaves:cooked_dinosaur_chop");
+});
+
+StartupEvents.modifyCreativeTab("domesticationinnovation:domesticationinnovation", event => {
+    event.setIcon("domesticationinnovation:pet_bed_light_gray");
+});
+
+StartupEvents.modifyCreativeTab("farmersdelight:farmersdelight", event => {
+    event.setIcon("farmersdelight:cooking_pot");
+});
